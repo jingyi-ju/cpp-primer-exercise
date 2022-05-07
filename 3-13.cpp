e7d541ca29389a4dc99b8537b5ec77b0e93646ec
jingyi-ju <96923360+jingyi-ju@users.noreply.github.com>
2022-05-07 12:43:01 -0500

    vector initialization

---
+++ 3-13.cpp
@@ -0,0 +1,5 @@
+//3.13: how many elements are in these vectors? what are the values of the elements?
+
+//(b) and (f) are equivalent -> 10 elements initialzied empty strings
+vector<int> v2(10);
+vector<string> v6{10};
