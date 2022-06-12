12681b94fa30f0a5731e4893eaa7846c81a6fe65
jingyi-ju <96923360+jingyi-ju@users.noreply.github.com>
2022-06-11 20:39:22 -0500

    Adds explanation of erase-remove idiom and ::ispun

--- 3-10.cpp
+++ 3-10.cpp
@@ -4,6 +4,10 @@
 
 //3.10 read string, output string with punctuations removed
 string removePunct(string s){
+    /* erase-remove_if idiom: remove-if moves all elements which matches condition to the end, and 
+    returns the position of the first matching element; erase then remove starting from this position
+    to the end of the string */
+    /* use :: before ispunct(),isspace() and isdigit() to not have them interpreted as std::ispunct(), etc.*/
     for(auto &c:s) if(ispunct(c)) s.erase(remove_if(s.begin(), s.end(), ::ispunct), s.end());
     return s;
 }
